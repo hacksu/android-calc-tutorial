c17add3551443aec8278dcbfe73aa2811378d205
Brojowski <alex.gajowski@gmail.com>
2017-11-03 19:26:15 -0400

    Add TODOs for following along

--- src/app/src/main/java/com/hyland/calculatoroutline/CalculatorMainActivity.java
+++ src/app/src/main/java/com/hyland/calculatoroutline/CalculatorMainActivity.java
@@ -27,7 +27,7 @@ public class CalculatorMainActivity extends AppCompatActivity
 
         if (savedInstanceState != null)
         {
-
+            // TODO 15: Restore variables from rotation.
         }
 
         setupNumbers();
@@ -44,42 +44,50 @@ public class CalculatorMainActivity extends AppCompatActivity
     {
         super.onSaveInstanceState(outState);
 
+        // TODO 14: Save variables for rotation.
     }
 
     private void setupNumbers()
     {
-
+        // TODO 1: Number Handlers
     }
 
     private void setupAdd()
     {
-
+        // TODO 5: Add button handler
     }
 
     private void setupSubtract()
     {
-
+        // You can implement this on your own.
     }
 
     private void setupMultiply()
     {
-
+        // You can implement this on your own.
     }
 
     private void setupDivide()
     {
-
+        // You can implement this on your own.
     }
 
     private void setupClear()
     {
-
+        // TODO 13: Reset the calculator with the clear button.
     }
 
     private void setupTotal()
     {
-
+        // TODO 12: Set up equals button to do the calculation.
     }
+
+    // TODO 6: Create calculate stub.
+    {
+        // TODO 9: Get left and right operands.
+        // TODO 11: Display the result.
+    }
+
 
     private String getDisplayValue()
     {
@@ -95,12 +103,13 @@ public class CalculatorMainActivity extends AppCompatActivity
 
     private String performFunction(String leftOperand, String rightOperand, FunctionType functionType)
     {
+        // TODO 10: Do the calculation based on operator.
         return "";
     }
 
     private void concatDisplayView(String number)
     {
-
+        // TODO 7: 'Append to' or 'Overwrite' the existing number
     }
 
     private class NumberClickListener implements View.OnClickListener
@@ -109,13 +118,17 @@ public class CalculatorMainActivity extends AppCompatActivity
 
         public NumberClickListener(int buttonValue)
         {
-
+            // TODO 2: Save button value
         }
 
         @Override
         public void onClick(View view)
         {
+            // TODO 3: Show number when clicked.
 
+            // TODO 4: Show multi-digit numbers.
+
+            // TODO 8: Create a new number after pressing operator.
         }
     }
 }
